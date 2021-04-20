6f7ee5cbd47280bd23f0233f6ece9a75cf794ed4
Sachitha Hirushan <shpsachitha@gmail.com>
2021-04-20 16:27:20 +0530

    create new chat rooms done

---
+++ src/components/ChatList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="createChatRoom()" class="button">Create New Chat Room</button>
+  </div>
+</template>
+
+<script>
+import { db } from '../firebase';
+
+export default {
+  data() {
+    return { 
+        chats: [] 
+    }
+  },
+  firestore() {
+    return { 
+        chats: db.collection('chats').where('owner', '==', this.uid) 
+    }
+  },
+  methods: {
+      async createChatRoom() {
+          const newChat = await db.collection('chats').add({
+              createdAt: Date.now(),
+              owner: this.uid,
+              members: [this.uid]
+          })
+
+            console.log(newChat)
+      }
+
+
+      
+  },
+  props: ['uid']
+
+};
+</script>
