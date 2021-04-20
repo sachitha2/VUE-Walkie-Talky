47a416206b7aa3d95ba919c678ac2d22fb368b51
Sachitha Hirushan <shpsachitha@gmail.com>
2021-04-20 16:44:10 +0530

    query chat rooms done

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ Vue.use(firestorePlugin)
 import VueCompositionApi from '@vue/composition-api'
 Vue.use(VueCompositionApi)
 import Home from './components/Home'
+import ChatRoom from './components/ChatRoom'
 const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
+    { path: '/chats/:id', component: ChatRoom, name: 'chat' }
   ]
 })
 
