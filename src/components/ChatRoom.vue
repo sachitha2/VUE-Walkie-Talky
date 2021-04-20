50e35b178dd3211003cfbb926c559e1ebc142087
Sachitha Hirushan <shpsachitha@gmail.com>
2021-04-20 17:36:31 +0530

    query messages

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -3,18 +3,72 @@
     <h3>Welcome to ChatRoom.vue {{ chatId }}</h3>
 
     <router-link to="/">Back</router-link>
+    <User #user="{ user }">
+        <ul>
+            <li v-for="message of messages" :key="message.id">
+                {{message.text}}
+            </li>
+        </ul>
+
+        <input class="input" type="text" v-model="newMessageText">
+        <button 
+            :disabled="!newMessageText || loading"
+            class="button is-success"
+            @click="addMessage(user.uid)"
+        >
+        Send
+        </button>
+    </User>
 
   </main>
 </template>
 
 <script>
+import User from './User.vue';
+import { db } from '../firebase';
 
 export default {
+    components: {
+        User,
+    },
+    data() {
+      return {
+          newMessageText: '',
+          loading: false,
+          messages: [],
+      }
+  },
   computed: {
     chatId() {
       return this.$route.params.id;
     },
+    messagesCollection() {
+      return db.doc(`chats/${this.chatId}`).collection('messages');
+    },
   },
+
+  firestore() {
+    return {
+      messages: this.messagesCollection.orderBy('createdAt').limitToLast(10)
+    };
+  },
+   methods: {
+      async addMessage(uid) {
+
+          this.loading = true;
+
+          const { id: messageId } = this.messagesCollection.doc();
+
+           await this.messagesCollection.doc(messageId).set({
+              text: this.newMessageText,
+              sender: uid,
+              createdAt: Date.now(),
+
+          });
+          this.loading = false;
+          this.newMessageText = '';
+      }
+    },
 
 };
 </script>
